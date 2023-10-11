e802c9e8c0cc4f81a0c3e624089b192c64b13673
Nadiia-Manakova <nadiiamanakova33@gmail.com>
2023-10-11 21:14:09 +0300

    1st task finished

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,40 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+// Change code below this line
+const galleryList = document.querySelector(`.gallery`);
+
+function createGallery(arr) {
+    return arr.map(({ preview, original, description }) => `
+   <li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>`).join('');
+}
+
+galleryList.insertAdjacentHTML(`afterbegin`, createGallery(galleryItems));
+galleryList.addEventListener(`click`, handlerClick);
+
+function handlerClick(evt) {
+  const target = evt.target;
+  if (target.classList.contains('gallery__image')) {
+    
+    const modalImage = target.dataset.source;
+       const instance = basicLightbox.create(`
+    <img src="${modalImage}"  width="800" height="600">
+`)
+
+    instance.show();
+  document.addEventListener('keydown', keyDown);
+    function keyDown(event) {
+      if (event.code === 'Escape') {
+        instance.close();
+      }
+    }
+  }
+}
